9e407f74d69ec709a628f061d68d1a1904ca8068
Exdia <exd1a+github@proton.me>
2024-07-23 22:04:24 +0300

    Remove circular dependency

--- src/main/java/org/georgievbozhidar/softunifinal2/service/impl/UserServiceImpl.java
+++ src/main/java/org/georgievbozhidar/softunifinal2/service/impl/UserServiceImpl.java
@@ -1,8 +1,5 @@
 package org.georgievbozhidar.softunifinal2.service.impl;
 
-import org.georgievbozhidar.softunifinal2.entity.dto.CreateChainDTO;
-import org.georgievbozhidar.softunifinal2.entity.dto.CreateLocationDTO;
-import org.georgievbozhidar.softunifinal2.entity.dto.UserRegisterDTO;
 import org.georgievbozhidar.softunifinal2.entity.model.*;
 import org.georgievbozhidar.softunifinal2.repository.*;
 import org.georgievbozhidar.softunifinal2.service.*;
@@ -13,25 +10,26 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-    private final ChainRepository chainRepository;
-    private final LocationRepository locationRepository;
-    private final FoodRepository foodRepository;
-    private final DrinkRepository drinkRepository;
-    private final ChainService chainService;
-    private final LocationService locationService;
-    private final FoodService foodService;
-    private final DrinkService drinkService;
+//    private final ChainRepository chainRepository;
+//    private final LocationRepository locationRepository;
+//    private final FoodRepository foodRepository;
+//    private final DrinkRepository drinkRepository;
+//    private final ChainService chainService;
+//    private final LocationService locationService;
+//    private final FoodService foodService;
+//    private final DrinkService drinkService;
 
-    public UserServiceImpl(UserRepository userRepository, ChainRepository chainRepository, LocationRepository locationRepository, FoodRepository foodRepository, DrinkRepository drinkRepository, ChainService chainService, LocationService locationService, FoodService foodService, DrinkService drinkService) {
+//    public UserServiceImpl(UserRepository userRepository, ChainRepository chainRepository, LocationRepository locationRepository, FoodRepository foodRepository, DrinkRepository drinkRepository, ChainService chainService, LocationService locationService, FoodService foodService, DrinkService drinkService) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.chainRepository = chainRepository;
-        this.locationRepository = locationRepository;
-        this.foodRepository = foodRepository;
-        this.drinkRepository = drinkRepository;
-        this.chainService = chainService;
-        this.locationService = locationService;
-        this.foodService = foodService;
-        this.drinkService = drinkService;
+//        this.chainRepository = chainRepository;
+//        this.locationRepository = locationRepository;
+//        this.foodRepository = foodRepository;
+//        this.drinkRepository = drinkRepository;
+//        this.chainService = chainService;
+//        this.locationService = locationService;
+//        this.foodService = foodService;
+//        this.drinkService = drinkService;
     }
 
     @Override
