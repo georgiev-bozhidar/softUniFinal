26fa200ce98738ab7193fff4317503d276e94992
Exdia <exd1a+github@proton.me>
2024-07-23 22:05:39 +0300

    Add model attributes to location and chain

--- src/main/java/org/georgievbozhidar/softunifinal2/controller/ChainController.java
+++ src/main/java/org/georgievbozhidar/softunifinal2/controller/ChainController.java
@@ -7,7 +7,11 @@ import org.georgievbozhidar.softunifinal2.service.ChainService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 import java.util.Set;
@@ -19,6 +23,11 @@ public class ChainController {
 
     public ChainController(ChainService chainService) {
         this.chainService = chainService;
+    }
+
+    @ModelAttribute("chainCreationData")
+    public CreateChainDTO createChainCreationData(){
+        return new CreateChainDTO();
     }
 
     @GetMapping("/{id}")
--- src/main/java/org/georgievbozhidar/softunifinal2/controller/LocationController.java
+++ src/main/java/org/georgievbozhidar/softunifinal2/controller/LocationController.java
@@ -5,16 +5,22 @@ import org.georgievbozhidar.softunifinal2.entity.model.Location;
 import org.georgievbozhidar.softunifinal2.service.LocationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@RestController
 @RequestMapping("/location")
 public class LocationController {
     private final LocationService locationService;
 
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
+    }
+
+    @ModelAttribute("locationCreationData")
+    public CreateLocationDTO createLocationCreationData(){
+        return new CreateLocationDTO();
     }
 
     @GetMapping("/{id}")
