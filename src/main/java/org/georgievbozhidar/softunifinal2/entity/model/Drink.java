e346e8206a0f7739c95dec4c722552ec0722f067
Exdia <exd1a+github@proton.me>
2024-07-28 22:59:38 +0300

    Fix drink model not having getters and setters for name

--- src/main/java/org/georgievbozhidar/softunifinal2/entity/model/Drink.java
+++ src/main/java/org/georgievbozhidar/softunifinal2/entity/model/Drink.java
@@ -6,9 +6,22 @@ import org.georgievbozhidar.softunifinal2.entity.enums.DrinkType;
 @Entity
 @Table(name = "drinks")
 public class Drink extends Consumable {
+    @Column(nullable=false)
+    private String name;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private DrinkType drinkType;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public DrinkType getDrinkType() {
         return drinkType;
