bd652cebd8e73deb18fae1956a0d8717866551c4
Exdia <exd1a+github@proton.me>
2024-07-28 23:29:03 +0300

    Fix initialization roles and users being out of order

---
+++ src/main/java/org/georgievbozhidar/softunifinal2/init/Initialize.java
@@ -0,0 +1,62 @@
+package org.georgievbozhidar.softunifinal2.init;
+
+import org.georgievbozhidar.softunifinal2.entity.enums.Roles;
+import org.georgievbozhidar.softunifinal2.entity.model.Role;
+import org.georgievbozhidar.softunifinal2.repository.RoleRepository;
+import org.georgievbozhidar.softunifinal2.repository.UserRepository;
+import org.georgievbozhidar.softunifinal2.service.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class Initialize implements CommandLineRunner {
+    private final RoleRepository roleRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Initialize(RoleRepository roleRepository, UserService userService, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userService = userService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        initRoles();
+        initUsers();
+    }
+
+    public void initRoles(){
+        if (roleRepository.count() > 0) {
+            return;
+        }
+
+        Roles[] rolesArray = Roles.values();
+
+        for (Roles roles : rolesArray) {
+            Role role = new Role();
+            role.setRoles(roles);
+            roleRepository.save(role);
+        }
+    }
+
+    public void initUsers(){
+        if (userRepository.count() > 0){
+            return;
+        }
+
+        userService.createUser(
+                "admin",
+                passwordEncoder.encode("admin"),
+                "admin@admin",
+                LocalDate.of(2001, 1, 1),
+                List.of(roleRepository.findByRoles(Roles.ADMIN), roleRepository.findByRoles(Roles.USER))
+        );
+    }
+}
